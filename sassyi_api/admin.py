8941693eb8a25441c4fed21c3adb5a852a350920
Keegan Crankshaw <kcranky@gmail.com>
2019-09-24 20:26:56 +0200

    added more models to the admin panel

--- sassyi_api/admin.py
+++ sassyi_api/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Device, Activity, Scan
+from .models import Device, Activity, Scan, Card, Profile
 
 admin.site.register(Device)
 admin.site.register(Activity)
 admin.site.register(Scan)
+admin.site.register(Card)
+admin.site.register(Profile)
+
